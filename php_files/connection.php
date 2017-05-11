7b1a4098f696f0f4620c2c774419234788d19071
Harshit Kandhway <kandhway.harshit198@gmail.com>
2017-05-11 23:21:55 +0530

    Create connection.php

---
+++ php_files/connection.php
@@ -0,0 +1,8 @@
+<?php
+define('hostname','localhost');
+define('user','root');
+define('password','');
+define('databaseName','vitaran');
+$connect = mysqli_connect(hostname,user,password,databaseName);
+
+?>
